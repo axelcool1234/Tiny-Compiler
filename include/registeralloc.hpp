e121bd5735575716b86f511ea24209adbef9e271
Radhakrishna Vempati <vempatir@uci.edu>
2024-05-21 09:58:14 -0700

    registerAllocater files intialized

---
+++ include/registeralloc.hpp
@@ -0,0 +1,16 @@
+#ifndef REGISTERALLOC_HPP
+#define REGISTERALLOC_HPP
+#include "intermediaterepresentation.hpp"
+#include <unordered_set>
+#include <fstream>
+
+
+
+class RegisterAlloc {
+public:
+    IntermediateRepresentation ir;
+    RegisterAlloc(IntermediateRepresentation&& ir);
+    std::ofstream ofile;
+};
+
+#endif
---
+++ src/registeralloc.cpp
@@ -0,0 +1,8 @@
+#include "registeralloc.hpp"
+
+RegisterAlloc::RegisterAlloc(IntermediateRepresentation&& ir) : ir(std::move(ir)), ofile("a.out") {}
+
+
+
+
+
