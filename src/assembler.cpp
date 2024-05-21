800a602dc6cf9356fe1da37e9b80128c0abdba78
Brian Le <bkylele@gmail.com>
2024-05-21 15:32:38 -0700

    assembler can now make hello world :^)

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -1,77 +1,94 @@
 #include <cstdint>
+#include <cstring>
 #include <vector>
 #include <fstream>
 
 #include <elf.h>
-#include <string.h>
 
 constexpr size_t VADDR_START = 0x08048000;
+constexpr size_t NUM_SECTIONS = 2;
+
+
+Elf32_Ehdr init_elf32hdr() {
+    Elf32_Ehdr h;
+    std::memcpy(h.e_ident, "\x7f""ELF", 4);
+    h.e_ident[EI_CLASS] = ELFCLASS32;
+    h.e_ident[EI_DATA] = ELFDATA2LSB;
+    h.e_ident[EI_VERSION] = EV_CURRENT;
+    h.e_ident[EI_OSABI] = ELFOSABI_LINUX;
+    h.e_ident[EI_ABIVERSION] = 0;
+    std::memset(h.e_ident + EI_PAD, 0, EI_NIDENT - EI_PAD);
+
+    h.e_type = ET_EXEC;
+    h.e_machine = EM_386;
+    h.e_version = EV_CURRENT;
+    h.e_entry = VADDR_START + 0x1000;
+    h.e_phoff = sizeof(Elf32_Ehdr);
+    h.e_shoff = 0;
+    h.e_flags = 0;
+    h.e_ehsize = sizeof(Elf32_Ehdr);
+    h.e_phentsize = sizeof(Elf32_Phdr);
+    h.e_phnum = NUM_SECTIONS; // may change later for stack
+    h.e_shentsize = sizeof(Elf32_Shdr);
+    h.e_shnum = 0;
+    h.e_shstrndx = 0;
+
+    return h;
+}
 
 
 void write_elf()
 {
-    size_t program_offset{sizeof(Elf32_Ehdr) + sizeof(Elf32_Phdr)*2};
-    size_t program_size{program_offset};
+    // size_t program_offset{sizeof(Elf32_Ehdr) + sizeof(Elf32_Phdr)*NUM_SECTIONS};
+    // size_t program_size{program_offset};
 
-    Elf32_Ehdr elf_hdr;
-    elf_hdr.e_ident[EI_MAG0] = 0x7f;
-    elf_hdr.e_ident[EI_MAG1] = 'E';
-    elf_hdr.e_ident[EI_MAG2] = 'L';
-    elf_hdr.e_ident[EI_MAG3] = 'F';
-    elf_hdr.e_ident[EI_CLASS] = ELFCLASS32;
-    elf_hdr.e_ident[EI_DATA] = ELFDATA2LSB;
-    elf_hdr.e_ident[EI_VERSION] = EV_CURRENT;
-    elf_hdr.e_ident[EI_OSABI] = ELFOSABI_LINUX;
-    elf_hdr.e_ident[EI_ABIVERSION] = 0;
-    bzero(elf_hdr.e_ident + EI_PAD, EI_NIDENT - EI_PAD);
-
-    elf_hdr.e_type = ET_EXEC;
-    elf_hdr.e_machine = EM_386;
-    elf_hdr.e_version = EV_CURRENT;
-    elf_hdr.e_entry = VADDR_START + program_offset;
-    elf_hdr.e_phoff = sizeof(Elf32_Ehdr);
-    elf_hdr.e_shoff = 0;
-    elf_hdr.e_flags = 0;
-    elf_hdr.e_ehsize = sizeof(Elf32_Ehdr);
-    elf_hdr.e_phentsize = sizeof(Elf32_Phdr);
-    elf_hdr.e_phnum = 2; // may change later for stack
-    elf_hdr.e_shentsize = sizeof(Elf32_Shdr);
-    elf_hdr.e_shnum = 0;
-    elf_hdr.e_shstrndx = 0;
+    Elf32_Ehdr elf_hdr = init_elf32hdr();
 
     Elf32_Phdr text_hdr;
     text_hdr.p_type = PT_LOAD;
-    text_hdr.p_offset = program_offset;
-    text_hdr.p_vaddr = VADDR_START + program_offset;
+    text_hdr.p_offset = 0x1000;
+    text_hdr.p_vaddr = VADDR_START + 0x1000;
     text_hdr.p_paddr = 0;
-    text_hdr.p_filesz = program_size; // to be updated later
-    text_hdr.p_memsz = program_size;
-    text_hdr.p_flags = PF_X | PF_W | PF_R;
+    text_hdr.p_filesz = 0;  // to be updated later
+    text_hdr.p_memsz = 0;   // to be updated later
+    text_hdr.p_flags = PF_X | PF_R;
     text_hdr.p_align = 0x1000;
 
+    // Data section metadata depends on the size of the text section
     Elf32_Phdr data_hdr;
     data_hdr.p_type = PT_LOAD;
-    data_hdr.p_offset = program_offset;
-    data_hdr.p_vaddr = VADDR_START + program_offset;
+    data_hdr.p_offset = 0;  // to be updated later
+    data_hdr.p_vaddr = 0;   // to be updated later
     data_hdr.p_paddr = 0;
-    data_hdr.p_filesz = program_size; // to be updated later
-    data_hdr.p_memsz = program_size;
-    data_hdr.p_flags = PF_X | PF_W | PF_R;
+    data_hdr.p_filesz = 0;  // to be updated later
+    data_hdr.p_memsz = 0;   // to be updated later
+    data_hdr.p_flags = PF_W | PF_R;
     data_hdr.p_align = 0x1000;
 
-    std::vector<uint8_t> text{
+    std::vector<uint8_t> text {
         0xb8, 0x04, 0x00, 0x00, 0x00, // mov $4, %eax
         0xbb, 0x01, 0x00, 0x00, 0x00, // mov $1, %ebx
-        0xb9, 0x00, 0x80, 0x04, 0x08, // mov $0x08048000, %ecx (address of "Hello, world!\n")
+        0xb9, 0x00, 0xa0, 0x04, 0x08, // mov $0x08048000, %ecx (address of "Hello, world!\n")
         0xba, 0x0e, 0x00, 0x00, 0x00, // mov $14, %edx
         0xcd, 0x80,                   // int $0x80 (syscall)
         0xb8, 0x01, 0x00, 0x00, 0x00, // mov $1, %eax
-        0xbb, 0x02, 0x00, 0x00, 0x00, // mov $0, %ebx
+        0xbb, 0x2a, 0x00, 0x00, 0x00, // mov $0, %ebx
         0xcd, 0x80                    // int $0x80 (syscall)
     };
+    text_hdr.p_filesz = text.size();
+    text_hdr.p_memsz = text.size();
+    std::vector<uint8_t> text_padding(0x1000 - sizeof(Elf32_Ehdr) - NUM_SECTIONS * sizeof(Elf32_Phdr), 0);
 
-    const char* hello = "Hello, world!\n";
-    std::vector<uint8_t> data {hello, hello + strlen(hello)+1};
+
+    const char* msg = "Hello World\n";
+    size_t msg_len = std::strlen(msg);
+    data_hdr.p_offset = text_hdr.p_offset + text_hdr.p_memsz;
+    data_hdr.p_offset = data_hdr.p_offset + (0x1000 - data_hdr.p_offset % 0x1000);
+    data_hdr.p_vaddr = VADDR_START + data_hdr.p_offset;
+    data_hdr.p_filesz = msg_len;
+    data_hdr.p_memsz = msg_len;
+    std::vector<uint8_t> data_padding(0x1000 - text_hdr.p_memsz, 0);
+
 
     std::ofstream file("my.out", std::ios::binary);
 
@@ -82,19 +99,16 @@ void write_elf()
     file.write(reinterpret_cast<const char*>(&text_hdr), sizeof(text_hdr));
     file.write(reinterpret_cast<const char*>(&data_hdr), sizeof(data_hdr));
 
-    // // Pad to start of text segment (0x1000)
-    // std::vector<uint8_t> padding(0x1000 - sizeof(elf_hdr) - 2 * sizeof(Elf32_Phdr), 0);
-    // file.write(reinterpret_cast<const char*>(padding.data()), padding.size());
-
+    // write padding before text segment
+    file.write(reinterpret_cast<const char*>(text_padding.data()), text_padding.size());
     // Write text segment
     file.write(reinterpret_cast<const char*>(text.data()), text.size());
 
-    // // Pad to start of data segment (0x2000)
-    // padding = std::vector<uint8_t>(0x1000 - text.size(), 0);
-    // file.write(reinterpret_cast<const char*>(padding.data()), padding.size());
-
+    // write padding before data segment
+    file.write(reinterpret_cast<const char*>(data_padding.data()), data_padding.size());
     // Write data segment
-    file.write(reinterpret_cast<const char*>(data.data()), data.size());
+    file.write(reinterpret_cast<const char*>(msg), msg_len);
+    // file.write(reinterpret_cast<const char*>(data.data()), data.size());
 }
 
 
